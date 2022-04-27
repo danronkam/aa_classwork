1ec96084938ef244190d35e26fcd3d2503ca9483
danronkam <danronkam@gmail.com>
2022-04-26 17:48:14 -0700

    update

--- W2D2/startup_project/lib/employee.rb
+++ W2D2/startup_project/lib/employee.rb
@@ -1,3 +1,19 @@
 class Employee
+    def name
+        @name
+    end
 
+    def title
+        @title
+    end
+
+    def initialize(name, title)
+        @name = name
+        @title = title
+        @earnings = 0
+    end
+
+    def pay(amount)
+        @earnings += amount
+    end 
 end
--- W2D2/startup_project/lib/startup.rb
+++ W2D2/startup_project/lib/startup.rb
@@ -1,5 +1,29 @@
 require "employee"
 
 class Startup
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def name
+        @name
+    end
+
+    def funding 
+        @funding
+    end
+
+    def salaries
+        @salaries
+    end
+
+    def employee
+        @employee
+    end
+    
+
 
 end
