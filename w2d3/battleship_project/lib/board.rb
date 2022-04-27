fbb82eecfbe60d1ed999c271c646f4d2c71dea84
danronkam <danronkam@gmail.com>
2022-04-27 12:19:16 -0700

    lunch update

--- w2d3/battleship_project/lib/board.rb
+++ w2d3/battleship_project/lib/board.rb
@@ -53,26 +53,26 @@ class Board
     end
 
     
-
-    def hidden_ships_grid
-        @grid.map do |row|
-            row.map do |ele|
-                if ele == :S
-                    :N
-                else
-                    ele
-                end
-            end
-        end
-    end
     
-    def cheat
-        Board.print_grid(@grid)
-    end
+    # def hidden_ships_grid
+    #     @grid.map do |row|
+    #         row.map do |ele|
+    #             if ele == :S
+    #                 :N
+    #             else
+    #                 ele
+    #             end
+    #         end
+    #     end
+    # end
+    
+    # def cheat
+    #     Board.print_grid(@grid)
+    # end
 
-    def print
-        Board.print_grid(self.hidden_ships_grid)
-    end
+    # def print
+    #     Board.print_grid(self.hidden_ships_grid)
+    # end
 
 end
 
--- w2d3/battleship_project/lib/player.rb
+++ w2d3/battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+    def get_move
+        print 'enter a position with coordinates separated 
+        with a space like `4 7`' 
+        numbers = gets.chomp.split(' ').map(&:to_i)
+    end
 end
