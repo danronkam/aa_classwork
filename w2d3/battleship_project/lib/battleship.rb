81d1aa7881482ca24b62952b4a0e6b0f13ad0966
danronkam <danronkam@gmail.com>
2022-04-27 14:43:57 -0700

    damn

--- w2d3/battleship_project/lib/battleship.rb
+++ w2d3/battleship_project/lib/battleship.rb
@@ -2,5 +2,62 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = @board.size/2
+    end
+
+    def board
+        @board
+    end
+
+    def player 
+        @player
+    end
+
+    def start_game
+        @board.place_random_ships
+        print @board.num_ships
+        @board.print
+    end
+
+    def lose?
+        if @remaining_misses <= 0
+            print "you lose" 
+            true
+        else   
+            false
+        end
+    end
+
+    def win?
+        if @board.num_ships == 0 
+            print 'you win'
+            true
+        else
+            false
+        end
+    end
+
+    def game_over?
+        if lose? || win?
+            true
+        else
+            false
+        end
+    end
+
+    def turn
+        move = @player.get_move
+
+
+        if @board.attack(move) != true
+            @remaining_misses -= 1
+            end
+        @board.print
+        print @remaining_misses
+    end
+
 
 end
--- w2d3/battleship_project/lib/board.rb
+++ w2d3/battleship_project/lib/board.rb
@@ -1,4 +1,11 @@
 class Board
+    def self.print_grid(arr)
+        arr.each do |row|
+          puts row.join(" ")
+        end
+      end
+
+      
     def initialize(n)
         @grid = Array.new(n) {Array.new(n, :N)}
         @size = n*n
@@ -30,13 +37,14 @@ class Board
 
         if self[position] == :S
             self[position] = :H
-            print 'you sunk my battleship!'
+            puts 'you sunk my battleship!'
             return true
         else  
             self[position] = :X
             return false
         end
     end
+
 
     def place_random_ships
         max = size * 0.25
@@ -54,25 +62,25 @@ class Board
 
     
     
-    # def hidden_ships_grid
-    #     @grid.map do |row|
-    #         row.map do |ele|
-    #             if ele == :S
-    #                 :N
-    #             else
-    #                 ele
-    #             end
-    #         end
-    #     end
-    # end
+    def hidden_ships_grid
+        @grid.map do |row|
+            row.map do |ele|
+                if ele == :S
+                    :N
+                else
+                    ele
+                end
+            end
+        end
+    end
     
-    # def cheat
-    #     Board.print_grid(@grid)
-    # end
+    def cheat
+        Board.print_grid(@grid)
+    end
 
-    # def print
-    #     Board.print_grid(self.hidden_ships_grid)
-    # end
+    def print
+        Board.print_grid(self.hidden_ships_grid)
+    end
 
 end
 
