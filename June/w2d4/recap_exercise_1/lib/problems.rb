594d42c0e37ee74beacb61260bebdf7ea88ce50a
danronkam <danronkam@gmail.com>
2022-06-23 11:40:36 -0700

    mid day update

--- June/w2d4/recap_exercise_1/lib/problems.rb
+++ June/w2d4/recap_exercise_1/lib/problems.rb
@@ -6,7 +6,19 @@
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
 def all_vowel_pairs(words)
+    vowels = 'aeiou'
+    vowelArr = vowels.split(' ')
+    pairs = []
 
+        words.each_with_index do |word1, i|
+            words.each_with_index do |word2, j|
+                pair = word1 + ' ' + word2
+
+                pairs << pair if j > i && vowelArr.all? {|letter| pairs.include?(letter)}
+            end
+        end
+
+        pairs
 end
 
 
@@ -18,6 +30,12 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
+    (2...num).each do |factor|
+        if num % factor == 0
+            return true
+        end
+    end
+    return false
 
 end
 
@@ -32,7 +50,7 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-
+    bigrams.select { |bigram| str.include?(bigram) }
 end
 
 class Hash
@@ -50,7 +68,13 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
+        prc ||= Proc.new { |k, v| k == v }
 
+        newArr = {}
+        self.each do |k, v|
+            newArr[k] = v if prc.call(k, v)
+        end
+        newArr
     end
 end
 
@@ -64,6 +88,23 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
+        def substrings(length = nil)
+            subs = []
+    
+            (0...self.length).each do |start|
+                (start...self.length).each do |ending|
+                    sub = self[start..ending]
+                    subs << self[start..ending]
+                end
+            end
+    
+            if length.nil?
+                subs
+            else
+                subs.select { |sub| sub.length == length}
+            end
+        end
+        
 
     end
 
@@ -77,7 +118,18 @@ class String
     # "apple".caesar_cipher(1)    #=> "bqqmf"
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
-    def caesar_cipher(num)
+    def caesar_cipher(num)    
+        alphabet = ("a".."z").to_a
+        new_str = ""
+      
+        self.each_char do |char|
+          old_idx = alphabet.index(char)
+          new_idx = old_idx + num
+          new_char = alphabet[new_idx % 26]
+          new_str += new_char
+        end
+      
+        return new_str
 
     end
 end
--- June/w2d4/recursion_exercise/lib/problems.rb
+++ June/w2d4/recursion_exercise/lib/problems.rb
@@ -12,6 +12,8 @@
 # pow(3, 4) # => 81
 # pow(4, 3) # => 64
 def pow(base, exponent)
+    return 1 if exponent == 0
+    base * pow(base, exponent - 1)
 
 end
 
@@ -34,7 +36,14 @@ end
 # lucas_number(3)   # =>    4
 # lucas_number(5)   # =>    11
 # lucas_number(9)   # =>    76
+
+#lucas_numer(3) = 
 def lucas_number(n)
+    return 2 if n == 0
+    return 1 if n == 1
+
+    lucas_number(n-1) + lucas_number(n-2)
+    
 
 end
 
@@ -51,6 +60,10 @@ end
 # sum_array([5, 2])         # => 7
 # sum_array([4, 10, -1, 2]) # => 15
 def sum_array(array)
+   return 0 if array.empty?
+    first = array[0]
+    rest = array[1..-1]
+    first + sum_array(rest)
 
 end
 
@@ -67,7 +80,8 @@ end
 # reverse_string("internet")    # => "tenretni"
 # reverse_string("friends")     # => "sdneirf"
 def reverse_string(str)
-
+    return '' if str.empty?
+    reverse_string(str[1..-1]) + str[0]
 end
 
 
